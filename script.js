57e6675cb05aaa3a5433fd7318de72371f549a99
Alpha <j8ets9@gmail.com>
2020-05-17 22:45:03 +0300

    Первый комит

---
+++ script.js
@@ -0,0 +1,97 @@
+/*'use strick';
+
+let persone = {
+    name: "John",
+    age: 25, 
+    isMarried: false
+};
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[0]);*/
+/*
+//1
+console.log(typeof(String(4)));
+//2
+
+console.log(typeof(parseInt('15px', 10)));
+
+
+let ans = +prompt("Hello?", '');*/
+
+//0, '', null, undefined, NaN
+/*
+let switcher = null;
+
+if(switcher) {
+    console.log("Working..")
+}
+
+switcher = 1;
+
+if(switcher) {
+    console.log("Working..")
+}
+*/
+/*
+console.log(typeof(Boolean('5')));
+
+console.log(typeof(!!'5'));
+*/
+/*
+alert("Hello World!");
+*/
+/*
+let answer = confirm("Are you here?");
+console.log(answer);
+
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+let answer = prompt("Вам есть 18?", "Да", "");
+
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[0]);
+
+
+console.log(typeof(null));
+
+console.log("arr" + " - object");
+console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" == 2);
+
+let isCheked = false,
+    isClose = false;
+
+console.log(isCheked || isClose);
+
+
+console.log("arr" + " - object");
+*/
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+console.log("2" == 2);
+
+let isChecked = true,
+    isClose = false;
+
+    console.log(isChecked || isClose);
+
+
