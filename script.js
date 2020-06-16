b64aa26899237849ed46825fc86ee27870579467
Alpha <j8ets9@gmail.com>
2020-06-16 20:59:19 +0300

    Perviy commit

--- script.js
+++ script.js
@@ -1,101 +1,65 @@
-/*'use strick';
+'use strict'
+/*
+a = 5;
+console.log(a);
 
-let persone = {
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+
+console.log(4/0);
+console.log('String'*9);
+
+
+
+
+let person = {
     name: "John",
-    age: 25, 
+    age: 25,
     isMarried: false
-};
-console.log(persone["name"]);
-
-let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
-console.log(arr[0]);*/
-/*
-//1
-console.log(typeof(String(4)));
-//2
-
-console.log(typeof(parseInt('15px', 10)));
-
-
-let ans = +prompt("Hello?", '');*/
-
-//0, '', null, undefined, NaN
-/*
-let switcher = null;
-
-if(switcher) {
-    console.log("Working..")
 }
 
-switcher = 1;
-
-if(switcher) {
-    console.log("Working..")
-}
-*/
-/*
-console.log(typeof(Boolean('5')));
-
-console.log(typeof(!!'5'));
-*/
-/*
-alert("Hello World!");
-*/
-/*
-let answer = confirm("Are you here?");
-console.log(answer);
+console.log(person["name"]); 
 
 
 let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
-let answer = prompt("Вам есть 18?", "Да", "");
 
-
-let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
 console.log(arr[0]);
 
+//alert("Hello world!");
 
-console.log(typeof(null));
+//let answer = prompt("Are you +18?", "Yes", "No");
+console.log(typeof(arr));
 
+let answer = +prompt("Are you +18?", "Yes", "No");
+console.log(typeof(answer));
 console.log("arr" + " - object");
-console.log(4 + +" - object");
+console.log(4 + " - object");
+
 
 let incr = 10,
     decr = 10;
-
 
 
 console.log(incr++);
 console.log(decr--);
 
 console.log(5%2);
-console.log("2" == 2);
-
-let isCheked = false,
-    isClose = false;
-
-console.log(isCheked || isClose);
-
-
-console.log("arr" + " - object");
-
-
-let incr = 10,
-    decr = 10;
-
-
-console.log(incr);
-console.log(decr);
-
-console.log(5%2);
-console.log("2" == 2);
+console.log("2"== 2);
 
 let isChecked = true,
     isClose = false;
-
-    console.log(isChecked || isClose);
-
-
-
+console.log(isChecked || !isClose);
 */
 
 
+console.log(typeof(null));
+
+console.log("arr" + "- object");
+console.log(4 + " - object");
